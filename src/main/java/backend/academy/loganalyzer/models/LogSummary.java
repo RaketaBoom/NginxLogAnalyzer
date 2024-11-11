27eaebb0acee61204d3e3892db253001e6612093
RaketaBoom <77796518+RaketaBoom@users.noreply.github.com>
2024-11-11 23:06:06 +0400

    feat: add opportunity for adding LogSummary objects

--- src/main/java/backend/academy/loganalyzer/models/LogSummary.java
+++ src/main/java/backend/academy/loganalyzer/models/LogSummary.java
@@ -4,23 +4,25 @@ import backend.academy.loganalyzer.exceptions.EmptyLogException;
 import com.tdunning.math.stats.TDigest;
 import java.util.HashMap;
 import java.util.Map;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import static backend.academy.loganalyzer.constants.Constants.COMPRESSION_FACTOR;
 
 @Getter
+@AllArgsConstructor
 public class LogSummary {
     private int count;
     private double sum;
     private TDigest tDigest;
-    private Map<String, Integer> popularResources;
-    private Map<Integer, Integer> popularCodes;
+    private Map<String, Integer> resources;
+    private Map<Integer, Integer> codes;
 
     public LogSummary() {
         count = 0;
         sum = 0d;
         tDigest = TDigest.createDigest(COMPRESSION_FACTOR);
-        popularResources = new HashMap<>();
-        popularCodes = new HashMap<>();
+        resources = new HashMap<>();
+        codes = new HashMap<>();
     }
 
     public LogSummary addLog(Log logModel) {
@@ -31,8 +33,24 @@ public class LogSummary {
         sum += logModel.size();
         tDigest.add(logModel.size());
 
-        popularResources.compute(logModel.resource(), (key, value) -> value == null ? 1 : value + 1);
-        popularCodes.compute(logModel.code(), (key, value) -> value == null ? 1 : value + 1);
+        resources.compute(logModel.resource(), (key, value) -> value == null ? 1 : value + 1);
+        codes.compute(logModel.code(), (key, value) -> value == null ? 1 : value + 1);
+
+        return this;
+    }
+
+    public LogSummary add(LogSummary other) {
+        this.count += other.count;
+        this.sum += other.sum;
+        this.tDigest.add(other.tDigest);
+
+        other.resources.forEach((key, value) ->
+            this.resources.merge(key, value, Integer::sum)
+        );
+
+        other.codes.forEach((key, value) ->
+            this.codes.merge(key, value, Integer::sum)
+        );
 
         return this;
     }
