2d6deae9f6a2f0f6d74a94b660e670d690833803
RaketaBoom <77796518+RaketaBoom@users.noreply.github.com>
2024-11-12 17:47:23 +0400

    feat: change parameters of Report

--- src/main/java/backend/academy/loganalyzer/formatter/ReportFormatter.java
+++
@@ -1,7 +0,0 @@
-package backend.academy.loganalyzer.formatter;
-
-import backend.academy.loganalyzer.models.Report;
-
-public interface ReportFormatter {
-    void format(Report report, String path);
-}
--- src/main/java/backend/academy/loganalyzer/models/Report.java
+++ src/main/java/backend/academy/loganalyzer/models/Report.java
@@ -2,12 +2,13 @@ package backend.academy.loganalyzer.models;
 
 import backend.academy.loganalyzer.enums.Filter;
 import backend.academy.loganalyzer.enums.Format;
+import java.nio.file.Path;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
 
 public record Report(
-    List<String> files,
+    List<Path> files,
     Format format,
     LocalDate from,
     LocalDate to,
